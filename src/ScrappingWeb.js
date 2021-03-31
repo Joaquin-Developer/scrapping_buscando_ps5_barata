e43da1cfd51e9f9e8d9cef245e8677463d500206
Joaquin-Parrilla <joaquin.p.olivera@gmail.com>
2021-03-31 00:39:23 -0300

    actualizo

---
+++ src/ScrappingWeb.js
@@ -0,0 +1,15 @@
+const {Builder, By, Key, until} = require('selenium-webdriver');
+
+(async () => {
+    const driver = await new Builder().forBrowser('firefox').build();
+    try {
+        await driver.get('https://www.google.com');
+        // Enter text "cheese" and perform keyboard action "Enter"
+        await driver.findElement(By.name('q')).sendKeys('cheese', Key.ENTER);
+        const firstResult = await driver.wait(until.elementLocated(By.css('h3>div')), 10000);
+        console.log(await firstResult.getAttribute('textContent'));
+    }
+    finally{
+        driver.quit();
+    }
+})();
